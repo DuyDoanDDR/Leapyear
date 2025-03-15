4f41383806e78bbe057c6e4258b122d33986519f
Doan Minh Duy <duydoan812@gmail.com>
2025-03-15 16:00:18 +0700

    Finish Commit

---
+++ Leapyear/Program.cs
@@ -0,0 +1,18 @@
+﻿int year;
+Console.WriteLine("Year :");
+
+while (true)
+if (int.TryParse(Console.ReadLine(), out year) && year > 0)
+{
+    bool isleapyear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    if (isleapyear)
+    {
+        Console.WriteLine(year + " is a leap year\nSearch more years : ");
+    }
+    else Console.WriteLine(year + " is not a leap year\nSearch more years : ");
+}
+else
+{
+    Console.WriteLine(" \nEnter year as a positive integer !\nYear :");
+}
+
